c94808809d2a946e9e92189c641273f32f899322
ankit-mrexcalibur <55681020+ankit-mrexcalibur@users.noreply.github.com>
2021-12-31 19:59:20 +0530

    FinalCode

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Heading from "./Heading";
+import App from "./Components/App";
 
-ReactDOM.render(
-  <div>
-    <Heading />
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
